1a069e12dfb1d6fba88efe376a119c75793b71f3
Tanecia Thompson <taneciathompson@gmail.com>
2021-04-20 20:44:11 -0400

    added bcrypt

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-    'url' : 'mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    'dbName': 'wadeMongo'
-};
